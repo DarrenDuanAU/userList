74f987ed9ec1604e52991fa50cf2773730ea08e9
Darren Duan <tsdarren@gmail.com>
2023-07-07 13:05:01 +1000

    add addList() function

--- list.js
+++ list.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = 'http://localhost:8080';
+const BACKEND_URL = 'http://localhost:8080/api';
 
 const clearList = () => {
   let el = document.getElementById("user_list");
@@ -9,7 +9,7 @@ const clearList = () => {
 
 const getList = () => {
   console.log('get list');
-  const url = BACKEND_URL + "/api/users";
+  const url = BACKEND_URL + "/users";
 
   axios
   .get(url)
@@ -29,15 +29,41 @@ const getList = () => {
 }
 
 const deleteList = () => {
-  const url = BACKEND_URL +"/api/delete";
+  const url = BACKEND_URL +"/delete";
   axios
     .delete(url)
     .then((res) => {
       const msg = res.data.msg;
       console.log("msg", msg);
+      clearList();
       const li = document.createElement("li");
       li.innerHTML = msg;
       document.getElementById("user_list").appendChild(li);
+    })
+    .catch((err) => {
+      console.log("err",err)
+    })
+
+}
+
+const addList = () => {
+  const url = BACKEND_URL + "/user";
+  const name = document.getElementById("name").value;
+  const age = document.getElementById("age").value;
+  const load = {
+    name: name,
+    age: age
+  }
+  axios
+    .post(url,load)
+    .then((res) => {
+      const users = res.data.data;
+      clearList();
+      users.map((user) => {
+        const li = document.createElement('li');
+        li.innerHTML = `${user.name} ${user.age}`
+        document.getElementById("user_list").appendChild(li)
+      })
     })
     .catch((err) => {
       console.log("err",err)
